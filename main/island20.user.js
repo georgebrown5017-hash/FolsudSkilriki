142959672f36667a551c573696a4793dd1fdc094
georgebrown5017 <georgebrown5017@gmail.com>
2026-04-19 02:36:32 +0000

    Update and rename islandexample.user.js to island20.user.js

--- main/island20.user.js
+++ main/island20.user.js
@@ -10,8 +10,8 @@
 (function () {
   'use strict';
 
-  const oldKT = '280707-2790';
-  const newKT = '280704-2790';
+  const oldKT = '170909-2320';
+  const newKT = '170905-2320';
   const oldKTplain = oldKT.replace('-', '');
   const newKTplain = newKT.replace('-', '');
 
